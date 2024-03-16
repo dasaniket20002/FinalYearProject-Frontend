51a0f29e73d101f45de2d40569bcd8423efea277
Aniket Das <47720941+dasaniket20002@users.noreply.github.com>
2024-03-16 15:04:21 +0530

    fresh signup page start

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -5,37 +5,7 @@ import InputField from "./misc/InputField";
 import axios from "axios";
 
 const Home = () => {
-	const jwtToken = localStorage.getItem("JWT");
-	const accessToken = localStorage.getItem("ACCESS_TOKEN");
-	const UserObject: ServerResponseJWTDecoded = jwtToken
-		? jwtDecode(jwtToken)
-		: {
-				_id: "default",
-				name: "Guest",
-				email: "default",
-		  };
-
 	const [search, setSearch] = useState<string>("");
-
-	const [videosList, setVideosList] = useState<VideosResponse>();
-
-	useEffect(() => {
-		const serverIP = "http://localhost:5001/";
-		const youtubeFunctionLinks = {
-			trending: serverIP + "youtube/trending",
-			search: serverIP + "youtube/search",
-		};
-
-		let link = youtubeFunctionLinks.trending;
-		if (accessToken) {
-			link += "?access_token=" + accessToken;
-		}
-		console.log(link);
-		axios.get(link).then((res) => {
-			console.log(res);
-			setVideosList(res.data);
-		});
-	}, [accessToken]);
 
 	return (
 		<div className="pt-[6rem] min-h-[calc(50vh+6rem)]">
@@ -52,8 +22,6 @@ const Home = () => {
 					className="text-white w-full md:w-1/2"
 				/>
 			</form>
-
-			{videosList?.video_list?.map((item) => item.id)}
 		</div>
 	);
 };
